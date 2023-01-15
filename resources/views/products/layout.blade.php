4ae5437b96a8d41593f6cd496d72507d83cdb2bb
Fuck the Error! <74085442+Kaung-Myat-Soe@users.noreply.github.com>
2023-01-15 12:55:41 +0630

    crud

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Laravel CRUD</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- <link rel="stylesheet" href="../resources/css/app.css"> -->
+        
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+   <style>
+   
+   </style>
+    </head>
+    <body>
+        
+       <div class="container">
+        <br>
+        @yield('content')
+        
+       </div>
+        
+       
+    </body>
+</html>
